a112773cda692ee32afa64f2ec134adf348368ef
lirongtong <lirongtong@tvflnet.com>
2018-06-19 19:27:19 +0800

    config
    环境配置文件

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict';
+const merge = require('webpack-merge');
+const prodEnv = require('./prod.env');
+
+module.exports = merge(prodEnv, {
+    NODE_ENV: '"development"',
+    SERVER_NAME: '"http://localhost:8080/api/v1"'
+});
---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict';
+module.exports = {
+    NODE_ENV: '"production"',
+    SERVER_NAME: '"http://api.tvflnet.com/cms/v1"'
+};
