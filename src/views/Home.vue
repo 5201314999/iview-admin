d8fe0f05cdd3c417fa7a0ea95bd371640cca7027
lirongtong <lirongtong@tvflnet.com>
2018-06-19 19:43:40 +0800

    views
    视图

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+
+    const HomeComponent = {
+        mounted() {
+            this.$api.get('user/1');
+        }
+    };
+
+    export default HomeComponent;
+
+</script>
+
---
+++ src/views/Login.vue
@@ -0,0 +1,11 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+
+    const LoginComponent = {};
+
+    export default LoginComponent;
+
+</script>
