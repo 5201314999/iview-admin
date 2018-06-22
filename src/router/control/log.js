67b66c378808a109cd3a2df2c70dc12fbc7f295a
lirongtong <lirongtong@tvflnet.com>
2018-06-22 20:43:26 +0800

    1. 样式修正
    2. 新增两个测试文件 [ preview.js / log.js ]

---
+++ src/router/control/log.js
@@ -0,0 +1,13 @@
+/**
+ * @project app
+ * @author lirongtong <lirongtong@hotmail.com>
+ * @github: https://github.com/wildidea
+ * @date 2018-6-21 15:16
+ */
+
+import Log from '../../views/control/Log';
+
+export default {
+    path: 'log',
+    component: Log
+}
