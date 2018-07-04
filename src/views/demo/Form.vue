61373c1bbdeaa7244235a22a2f83193a833b425f
lirongtong <lirongtong@tvflnet.com>
2018-07-04 10:44:48 +0800

    1. 详情label宽度修正
    2. 表单 [ 输入框 / 单选 / 多选 ]

--- src/views/demo/Form.vue
+++ src/views/demo/Form.vue
@@ -1,15 +1,37 @@
 <template>
     <div class="fl-container">
         <div class="fl-content">
-            <Form :model="form.data" :rules="form.rules">
-                <FormItem prop="name" label="推荐组名称">
+            <Form :model="form.validate" :rules="form.rules">
+                <FormItem prop="name" label="推荐组名称" class="clearfix">
                     <Row class="form-input">
                         <Col>
-                            <Input type="text" v-model="form.data.name" placeholder="输入框提示" size="large" />
+                            <Input type="text" v-model="form.validate.name" placeholder="输入框提示" size="large" />
                         </Col>
                     </Row>
                 </FormItem>
-                <FormItem prop="title" label="展示组标题">
+                <FormItem prop="title" label="展示组标题" class="clearfix">
+                    <div class="fl-radio">
+                        <RadioGroup v-model="form.validate.show">
+                            <Radio label="1"><span>是</span></Radio>
+                            <Radio label="0"><span>否</span></Radio>
+                        </RadioGroup>
+                    </div>
+                </FormItem>
+                <FormItem prop="range" label="适用范围" class="clearfix">
+                    <div class="fl-checkbox">
+                        <CheckboxGroup v-model="form.validate.range">
+                            <Checkbox label="desktop">
+                                <span>桌面端</span>
+                                <icon type="ios-checkmark-empty"></icon>
+                                <span class="fl-checkbox-arrow"></span>
+                            </Checkbox>
+                            <Checkbox label="mobile">
+                                <span>手机端</span>
+                                <icon type="ios-checkmark-empty"></icon>
+                                <span class="fl-checkbox-arrow"></span>
+                            </Checkbox>
+                        </CheckboxGroup>
+                    </div>
                 </FormItem>
             </Form>
         </div>
@@ -22,16 +44,17 @@
         data() {
             return {
                 form: {
-                    data: {
-                        name: ''
+                    validate: {
+                        name: '',
+                        title: '',
+                        show: '1',
+                        range: []
                     },
                     rules: {
                         name: [{required: true, message: '推荐组名称不能为空', trigger: 'blur'}],
-                        title: [{required: true, message: '请选择推荐组标题'}]
-                    },
-                    radio: [
-
-                    ]
+                        title: [{required: true, trigger: 'click', message: '请选择是否展示推荐组标题',}],
+                        range: [{required: true}]
+                    }
                 }
             }
         }
