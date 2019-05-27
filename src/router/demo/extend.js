7d8fc2b453c201847f2e9e3478c031817002e50f
liyongjie <liyongjie@tvflnet.com>
2019-05-27 11:32:37 +0800

    删除拖拽组件

--- src/router/demo/extend.js
+++ src/router/demo/extend.js
@@ -1,7 +1,6 @@
 import Extend from '@/views/demo/extend/Main';
 import ExtendUpload from '@/views/demo/extend/Upload';
 import ExtendTransfer from '@/views/demo/extend/Transfer';
-import ExtendDraggable from '@/views/demo/extend/Draggable';
 
 export default {
     path: 'extend',
@@ -11,6 +10,5 @@ export default {
         {path: '/', component: ExtendUpload, redirect: '/extend/upload'},
         {path: 'upload', component: ExtendUpload, name: 'Upload 上传'},
         {path: 'transfer', component: ExtendTransfer, name: 'Transfer 穿梭框'},
-        {path: 'draggable', component: ExtendDraggable, name: 'Draggable 拖拽'}
     ]
 }
