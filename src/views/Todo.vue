2d7490dd33728b4c7d7d84e6f22c4a475c55ca44
5201314999 <1446418561@qq.com>
2019-05-28 09:22:53 +0800

    增加了todo 页面，第一版

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -5,15 +5,37 @@ Description
 @version 1.0.0
 -->
 <template>
-  <div>
-        不好意思，该页有待开发
+  <div class="tip-wrapper">
+      <h1>~页面正在开发</h1>
+      <div class="group">
+        <h2 class="sub-title">别担心上车，我带你回去</h2>
+        <Button type="primary">返回首页</Button>
+      </div>
+      <img src="../assert/images/todo.png">
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+  }
 </script>
 
 <style lang="scss" scoped>
-  
+    .tip-wrapper{
+        min-height:400px;
+        text-align: center;
+        color:#1B8EE4;
+        .group{
+            margin-top:40px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .sub-title{
+                margin-right:16px;
+            }
+        }
+        img{
+            margin-top:40px;
+        }
+    }
 </style>
